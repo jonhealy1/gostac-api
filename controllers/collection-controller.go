f0bb20b8197ad6a59bd762a5f46cafd3e92a8683
jonhealy1 <jonathan.d.healy@gmail.com>
2022-09-02 02:58:06 -0700

    add root route

--- controllers/collection-controller.go
+++ controllers/collection-controller.go
@@ -1,1 +1,35 @@
 package controllers
+
+import (
+	"go-stac-api-postgres/models"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func Root(c *fiber.Ctx) error {
+	links := []models.Link{
+		{
+			Rel:   "self",
+			Type:  "application/json",
+			Href:  "/",
+			Title: "root catalog",
+		},
+		{
+			Rel:   "children",
+			Type:  "application/json",
+			Href:  "/collections",
+			Title: "stac child collections",
+		},
+	}
+
+	rootCatalog := models.Root{
+		Id:          "test-catalog",
+		StacVersion: "1.0.0",
+		Description: "test catalog for go-stac-api, please edit",
+		Title:       "go-stac-api",
+		Links:       links,
+	}
+
+	return c.Status(http.StatusOK).JSON(&fiber.Map{"data": rootCatalog})
+}
--- router/item_routes.go
+++ router/item_routes.go
@@ -1,16 +1,14 @@
 package routes
 
 import (
-	"go-stac-api-postgres/controllers"
-
 	"github.com/gofiber/fiber/v2"
 )
 
 func ItemRoute(app *fiber.App) {
-	app.Post("/collections/:collectionId/items", controllers.CreateItem)
-	app.Get("/collections/:collectionId/items/:itemId", controllers.GetItem)
-	app.Get("/collections/:collectionId/items", controllers.GetItemCollection)
-	app.Put("/collections/:collectionId/items/:itemId", controllers.EditItem)
-	app.Delete("/collections/:collectionId/items/:itemId", controllers.DeleteItem)
-	app.Post("/search", controllers.PostSearch)
+	// app.Post("/collections/:collectionId/items", controllers.CreateItem)
+	// app.Get("/collections/:collectionId/items/:itemId", controllers.GetItem)
+	// app.Get("/collections/:collectionId/items", controllers.GetItemCollection)
+	// app.Put("/collections/:collectionId/items/:itemId", controllers.EditItem)
+	// app.Delete("/collections/:collectionId/items/:itemId", controllers.DeleteItem)
+	// app.Post("/search", controllers.PostSearch)
 }
