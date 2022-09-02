a1c13348af52bc5cd695e20aaed4d29d11d5a97e
jonhealy1 <jonathan.d.healy@gmail.com>
2022-09-02 02:24:48 -0700

    add routes

--- router/collection_routes.go
+++ router/collection_routes.go
@@ -1,1 +1,17 @@
 package routes
+
+import (
+	"go-stac-api-postgres/controllers"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func CollectionRoute(app *fiber.App) {
+	app.Get("/", controllers.Root)
+	app.Get("/conformance", controllers.Conformance)
+	app.Post("/collections", controllers.CreateCollection)
+	app.Get("/collections/:collectionId", controllers.GetCollection)
+	app.Put("/collections/:collectionId", controllers.EditCollection)
+	app.Delete("/collections/:collectionId", controllers.DeleteCollection)
+	app.Get("/collections", controllers.GetCollections)
+}
