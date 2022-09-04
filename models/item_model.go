d06c8c086f6887a5653354fe42b5e2cc46178916
jonhealy1 <jonathan.d.healy@gmail.com>
2022-09-04 08:31:00 -0700

    POST item

--- models/item_model.go
+++ models/item_model.go
@@ -1,19 +1,19 @@
 package models
 
 import (
-	"github.com/lib/pq"
 	"gorm.io/gorm"
 )
 
 type Item struct {
 	gorm.Model
 
-	Id          string `json:"id,omitempty"`
-	Type        string `json:"type,omitempty"`
-	Collection  string `json:"collection,omitempty"`
-	StacVersion string `json:"stac_version,omitempty"`
+	Id string `json:"id,omitempty"`
+	// Type       string `json:"type,omitempty"`
+	// Collection string `json:"collection,omitempty"`
+	// StacVersion string `json:"stac_version,omitempty"`
 	// StacExtensions []string      `json:"stac_extensions,omitempty"`
-	Bbox pq.Float64Array `gorm:"type:float[]"`
+	// Bbox pq.Float64Array `gorm:"type:float[]"`
+	Data JSONB `gorm:"type:jsonb" json:"data,omitempty"`
 	// Geometry       interface{}   `json:"geometry,omitempty"`
 	// Properties     interface{}   `json:"properties,omitempty"`
 	// Assets         interface{}   `json:"assets,omitempty"`
--- router/item_routes.go
+++ router/item_routes.go
@@ -1,11 +1,13 @@
 package routes
 
 import (
+	"go-stac-api-postgres/controllers"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func ItemRoute(app *fiber.App) {
-	// app.Post("/collections/:collectionId/items", controllers.CreateItem)
+	app.Post("/collections/:collectionId/items", controllers.CreateItem)
 	// app.Get("/collections/:collectionId/items/:itemId", controllers.GetItem)
 	// app.Get("/collections/:collectionId/items", controllers.GetItemCollection)
 	// app.Put("/collections/:collectionId/items/:itemId", controllers.EditItem)
