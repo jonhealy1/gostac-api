a4fff777c48aec76602d584e7e43ade8c11454f1
jonhealy1 <jonathan.d.healy@gmail.com>
2022-09-05 06:04:28 -0700

    search route

--- controllers/search-controller.go
+++ controllers/search-controller.go
@@ -1,1 +1,16 @@
 package controllers
+
+import "github.com/gofiber/fiber/v2"
+
+// PostSearch godoc
+// @Summary POST Search request
+// @Description Search for STAC items via the Search endpoint
+// @Tags Search
+// @ID post-search
+// @Accept  json
+// @Produce  json
+// @Param search body models.Search true "Search body json"
+// @Router /search [post]
+func PostSearch(c *fiber.Ctx) error {
+	return nil
+}
