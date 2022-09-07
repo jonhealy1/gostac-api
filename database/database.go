91555e59f4f1267a1f34420ec1773c8d83e5e9d3
jonhealy1 <jonathan.d.healy@gmail.com>
2022-09-07 03:57:41 -0700

    get env variables

--- database/database.go
+++ database/database.go
@@ -1,13 +1,17 @@
 package database
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"go-stac-api-postgres/models"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+
+	"github.com/joho/godotenv"
 )
 
 type Dbinstance struct {
@@ -16,10 +20,22 @@ type Dbinstance struct {
 
 var DB Dbinstance
 
-// connectDb
-func ConnectDb() {
-	dsn := "host=localhost user=username password='password' dbname=postgis port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+func getEnv(key string) string {
+	err := godotenv.Load(".env")
 
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+func ConnectDb() {
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		getEnv("POSTGRES_HOST"), getEnv("POSTGRES_PORT"), getEnv("POSTGRES_USER"),
+		getEnv("POSTGRES_PASS"), getEnv("POSTGRES_DBNAME"), "disable",
+	)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
