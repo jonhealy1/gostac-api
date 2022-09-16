3c7673f3ef755d5a78937e8afd31651dc9077e35
jonhealy1 <jonathan.d.healy@gmail.com>
2022-09-16 12:35:36 +0300

    add caching

--- app.go
+++ app.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/etag"
 	"github.com/gofiber/fiber/v2/middleware/favicon"
@@ -16,9 +19,18 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New())
+	app.Use(cache.New())
 	app.Use(etag.New())
 	app.Use(favicon.New())
 	app.Use(logger.New())
+
+	app.Use(cache.New(cache.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return c.Query("refresh") == "true"
+		},
+		Expiration:   30 * time.Minute,
+		CacheControl: true,
+	}))
 
 	router.CollectionRoute(app)
 	router.ItemRoute(app)
