b653d68ef7a6f1d0a9547d71c845979abd8d0249
jonhealy1 <jonathan.d.healy@gmail.com>
2022-09-16 12:23:43 +0300

    add logger

--- app.go
+++ app.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 
 	database "go-stac-api-postgres/database"
 	router "go-stac-api-postgres/router"
@@ -13,6 +14,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New())
+	app.Use(logger.New())
 
 	router.CollectionRoute(app)
 	router.ItemRoute(app)
