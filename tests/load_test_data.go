e88a569a8aa6a3f327f81e2cde9401d5714a7da7
jonhealy1 <jonathan.d.healy@gmail.com>
2022-09-17 15:18:22 +0300

    load items, feature collections

--- tests/load_test_data.go
+++ tests/load_test_data.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -34,25 +35,34 @@ func LoadCollection() {
 	log.Printf(sb)
 }
 
-// func LoadItems() {
-// 	postBody, _ := json.Marshal(map[string]string{
-// 		"id":           "sentinel-s2-l2a-cogs",
-// 		"collection":   "sentinel-s2-l2a-cogs-testing",
-// 		"stac_version": "1.0.0",
-// 	})
-// 	responseBody := bytes.NewBuffer(postBody)
+func LoadItems() {
+	jsonFile, err := os.Open("setup_data/sentinel-s2-l2a-cogs_0_100.json")
 
-// 	resp, err := http.Post("http://localhost:6002/collections", "application/json", responseBody)
+	if err != nil {
+		fmt.Println(err)
+	}
+	byteValue, _ := ioutil.ReadAll(jsonFile)
 
-// 	if err != nil {
-// 		log.Fatalf("An Error Occured %v", err)
-// 	}
-// 	defer resp.Body.Close()
+	type FeatureCollection struct {
+		Type     string        `json:"type"`
+		Features []interface{} `json:"features"`
+	}
 
-// 	body, err := ioutil.ReadAll(resp.Body)
-// 	if err != nil {
-// 		log.Fatalln(err)
-// 	}
-// 	sb := string(body)
-// 	log.Printf(sb)
-// }
+	var fc FeatureCollection
+
+	json.Unmarshal(byteValue, &fc)
+
+	print(len(fc.Features))
+
+	var i int
+	for i < (len(fc.Features) - 50) {
+		test, _ := json.Marshal(fc.Features[i])
+		responseBody := bytes.NewBuffer(test)
+		resp, err := http.Post("http://localhost:6002/collections/sentinel-s2-l2a-cogs-test/items", "application/json", responseBody)
+		if err != nil {
+			log.Fatalf("An Error Occured %v", err)
+		}
+		defer resp.Body.Close()
+		i = i + 1
+	}
+}
