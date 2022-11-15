c0ef051e5fe87fd8d4dbab607420a534b50bb722
jonhealy1 <jonathan.d.healy@gmail.com>
2022-11-15 15:18:28 +0300

    search point, polygon

--- controllers/search-controller.go
+++ controllers/search-controller.go
@@ -1,12 +1,16 @@
 package controllers
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"go-stac-api-postgres/database"
 	"go-stac-api-postgres/models"
+	"log"
 	"net/http"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/spatial-go/geoos/geoencoding"
 )
 
 // PostSearch godoc
@@ -34,25 +38,58 @@ func PostSearch(c *fiber.Ctx) error {
 		limit = search.Limit
 	}
 
-	tx1 := database.DB.Db.Limit(limit)
-	tx2 := database.DB.Db.Limit(limit)
-	if len(search.Collections) > 0 {
-		tx1 = database.DB.Db.Limit(limit).Where("collection IN ?", search.Collections)
-		tx2 = tx1.Limit(limit)
-		fmt.Println(tx1)
-	}
+	// tx1 := database.DB.Db.Limit(limit)
+	// tx2 := database.DB.Db.Limit(limit)
+	// if len(search.Collections) > 0 {
+	// 	tx1 = database.DB.Db.Limit(limit).Where("collection IN ?", search.Collections)
+	// 	tx2 = tx1.Limit(limit)
+	// 	fmt.Println(tx1)
+	// }
 
-	if len(search.Ids) > 0 {
-		tx2 = tx1.Limit(limit).Where("id IN ?", search.Ids)
-		fmt.Println(tx1)
-	}
+	// if len(search.Ids) > 0 {
+	// 	tx2 = tx1.Limit(limit).Where("id IN ?", search.Ids)
+	// 	fmt.Println(tx1)
+	// }
 
-	err := tx2.Find(&items).Error
+	// err := tx2.Find(&items).Error
 
-	if err != nil {
-		c.Status(http.StatusBadRequest).JSON(
-			&fiber.Map{"message": "could not get items"})
-		return err
+	// if err != nil {
+	// 	c.Status(http.StatusBadRequest).JSON(
+	// 		&fiber.Map{"message": "could not get items"})
+	// 	return err
+	// }
+
+	geoString := ""
+	if search.Geometry.Type == "Point" || search.Geometry.Type == "Polygon" {
+		if search.Geometry.Type == "Point" {
+			geom := models.GeoJSONPoint{}.Coordinates
+			json.Unmarshal(search.Geometry.Coordinates, &geom)
+			geoString = fmt.Sprintf(`{"type":"Point", "Coordinates":[%f,%f]}`, geom[0], geom[1])
+			fmt.Println(geoString)
+		} else if search.Geometry.Type == "Polygon" {
+			geom := models.GeoJSONPolygon{}.Coordinates
+			json.Unmarshal(search.Geometry.Coordinates, &geom)
+			geoString = fmt.Sprintf(`{"type":"Polygon", "Coordinates":[[`)
+			for i := 0; i < len(geom[0])-1; i++ {
+				geoString += fmt.Sprintf("[%f,", geom[0][i][0])
+				geoString += fmt.Sprintf("%f],", geom[0][i][1])
+			}
+			geoString += fmt.Sprintf("[%f,", geom[0][len(geom[0])-1][0])
+			geoString += fmt.Sprintf("%f]", geom[0][len(geom[0])-1][1])
+			geoString += fmt.Sprintf("]]}")
+		}
+
+		buf := new(bytes.Buffer)
+		buf.Write([]byte(geoString))
+		got, err := geoencoding.Read(buf, geoencoding.GeoJSON)
+		if err != nil {
+			log.Println(err)
+		}
+		err = geoencoding.Write(buf, got, geoencoding.WKT)
+		if err != nil {
+			log.Println(err)
+		}
+		database.DB.Db.Raw("SELECT * FROM items WHERE ST_Intersects(items.geometry, ST_GeomFromText(?, 4326))", buf.String()).Scan(&items)
 	}
 
 	context := models.Context{
