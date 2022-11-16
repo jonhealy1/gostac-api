cc7573ee6417b80ccaaa458ab38c8af138546476
jonhealy1 <jonathan.d.healy@gmail.com>
2022-11-16 11:05:09 +0300

    search geometry no results

--- tests/search_routes_test.go
+++ tests/search_routes_test.go
@@ -146,3 +146,45 @@ func TestSearchGeometry(t *testing.T) {
 	assert.Equalf(t, 50, search_response.Context.Returned, "search geometry")
 	assert.Equalf(t, "sentinel-s2-l2a-cogs-test", search_response.Features[0].Collection, "search collections")
 }
+
+func TestSearchNoGeometry(t *testing.T) {
+	jsonBody := []byte(`{
+		"collections": ["sentinel-s2-l2a-cogs-test"],
+		"geometry": {
+			"type": "Polygon",
+        	"coordinates": [[
+				[70.8515625, -74.14512718337613],
+				[78.35937499999999, -74.14512718337613],
+				[78.35937499999999, -70.15296965617042],
+				[70.8515625, -70.15296965617042],
+				[70.8515625, -74.14512718337613]
+          	]]
+      	}
+	}`)
+	bodyReader := bytes.NewReader(jsonBody)
+
+	resp, err := http.Post(
+		"http://localhost:6002/search",
+		"application/json",
+		bodyReader,
+	)
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
+	}
+	defer resp.Body.Close()
+
+	assert.Equalf(t, 200, resp.StatusCode, "create item")
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var search_response responses.SearchResponse
+	json.Unmarshal(body, &search_response)
+
+	assert.Equalf(t, "item collection retrieved successfully", search_response.Message, "search geometry")
+	assert.Equalf(t, "FeatureCollection", search_response.Type, "search geometry")
+	assert.Equalf(t, 100, search_response.Context.Limit, "search geometry")
+	assert.Equalf(t, 0, search_response.Context.Returned, "search geometry")
+}
