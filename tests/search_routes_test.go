63876b57daa667fd278c6740a53f58e39de1401f
jonhealy1 <jonathan.d.healy@gmail.com>
2022-11-16 11:08:01 +0300

    search point tests

--- tests/search_routes_test.go
+++ tests/search_routes_test.go
@@ -188,3 +188,76 @@ func TestSearchNoGeometry(t *testing.T) {
 	assert.Equalf(t, 100, search_response.Context.Limit, "search geometry")
 	assert.Equalf(t, 0, search_response.Context.Returned, "search geometry")
 }
+
+func TestSearchPoint(t *testing.T) {
+	jsonBody := []byte(`{
+		"collections": ["sentinel-s2-l2a-cogs-test"],
+		"geometry": {
+			"type": "Point",
+			"coordinates": [177.064544, -72.690647]
+      	}
+	}`)
+	bodyReader := bytes.NewReader(jsonBody)
+
+	resp, err := http.Post(
+		"http://localhost:6002/search",
+		"application/json",
+		bodyReader,
+	)
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
+	}
+	defer resp.Body.Close()
+
+	assert.Equalf(t, 200, resp.StatusCode, "create item")
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var search_response responses.SearchResponse
+	json.Unmarshal(body, &search_response)
+
+	assert.Equalf(t, "item collection retrieved successfully", search_response.Message, "search geometry")
+	assert.Equalf(t, "FeatureCollection", search_response.Type, "search geometry")
+	assert.Equalf(t, 100, search_response.Context.Limit, "search geometry")
+	assert.Equalf(t, 49, search_response.Context.Returned, "search geometry")
+	assert.Equalf(t, "sentinel-s2-l2a-cogs-test", search_response.Features[0].Collection, "search collections")
+}
+
+func TestSearchNoPoint(t *testing.T) {
+	jsonBody := []byte(`{
+		"collections": ["sentinel-s2-l2a-cogs-test"],
+		"geometry": {
+			"type": "Point",
+			"coordinates": [77.064544, -72.690647]
+      	}
+	}`)
+	bodyReader := bytes.NewReader(jsonBody)
+
+	resp, err := http.Post(
+		"http://localhost:6002/search",
+		"application/json",
+		bodyReader,
+	)
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
+	}
+	defer resp.Body.Close()
+
+	assert.Equalf(t, 200, resp.StatusCode, "create item")
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var search_response responses.SearchResponse
+	json.Unmarshal(body, &search_response)
+
+	assert.Equalf(t, "item collection retrieved successfully", search_response.Message, "search geometry")
+	assert.Equalf(t, "FeatureCollection", search_response.Type, "search geometry")
+	assert.Equalf(t, 100, search_response.Context.Limit, "search geometry")
+	assert.Equalf(t, 0, search_response.Context.Returned, "search geometry")
+}
