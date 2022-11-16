de0aee255e67fba79127de7a578a6cbcb7e22041
jonhealy1 <jonathan.d.healy@gmail.com>
2022-11-16 19:20:48 +0300

    test item with no collection

--- tests/item_routes_test.go
+++ tests/item_routes_test.go
@@ -49,6 +49,40 @@ func TestCreateItem(t *testing.T) {
 	assert.Equalf(t, "success", collection_response.Message, "create item")
 }
 
+func TestCreateItemNoCollection(t *testing.T) {
+	var expected_item models.Item
+	jsonFile, err := os.Open("setup_data/S2B_1CCV_20181004_0_L2A-test")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, &expected_item)
+	responseBody := bytes.NewBuffer(byteValue)
+
+	resp, err := http.Post(
+		"http://localhost:6002/collections/sentinel-s2-l2a-cogs-test-x/items",
+		"application/json",
+		responseBody,
+	)
+	if err != nil {
+		log.Fatalf("An Error Occured %v", err)
+	}
+	defer resp.Body.Close()
+
+	assert.Equalf(t, 400, resp.StatusCode, "create item")
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var collection_response responses.CollectionResponse
+	json.Unmarshal(body, &collection_response)
+
+	assert.Equalf(t, "Collection does not exist", collection_response.Message, "create item")
+}
+
 func TestGetItem(t *testing.T) {
 	var expected_item models.Item
 	jsonFile, _ := os.Open("setup_data/S2B_1CCV_20181004_0_L2A-test.json")
