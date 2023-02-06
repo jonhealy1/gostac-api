4e886e8e23e14082b26e83d9961cea675cd33ac8
jonhealy1 <jonathan.d.healy@gmail.com>
2023-02-06 17:28:31 +0300

    finish re-factor

--- tests/search_routes_test.go
+++ tests/search_routes_test.go
@@ -5,12 +5,9 @@ import (
 	"encoding/json"
 	"go-stac-api-postgres/responses"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestSearchItems(t *testing.T) {
@@ -1029,26 +1026,45 @@ func TestGetSearchLine(t *testing.T) {
 }
 
 func TestGetSearchLineNoResults(t *testing.T) {
+	app := Setup()
 
-	resp, err := http.Get(
-		`http://localhost:6002/search?geometry={"type": "LineString","coordinates": [[19.85156249999997,-70.554563528593656],[11.101642,-75.690647]]}`,
-	)
+	url := "/search?geometry=" + url.QueryEscape(`{"type": "LineString","coordinates": [[19.85156249999997,-70.554563528593656],[11.101642,-75.690647]]}`)
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := app.Test(req, -1)
 	if err != nil {
-		log.Fatalf("An Error Occured %v", err)
+		t.Fatalf("An error occurred: %v", err)
 	}
 	defer resp.Body.Close()
 
-	assert.Equalf(t, 200, resp.StatusCode, "create item")
+	if resp.StatusCode != 200 {
+		t.Fatalf("Expected status code 200, but got %d", resp.StatusCode)
+	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalln(err)
+		t.Fatalf("An error occurred: %v", err)
 	}
 
-	var search_response responses.SearchResponse
-	json.Unmarshal(body, &search_response)
+	var searchResponse responses.SearchResponse
+	err = json.Unmarshal(body, &searchResponse)
+	if err != nil {
+		t.Fatalf("An error occurred: %v", err)
+	}
 
-	assert.Equalf(t, "FeatureCollection", search_response.Type, "search geometry")
-	assert.Equalf(t, 100, search_response.Context.Limit, "search geometry")
-	assert.Equalf(t, 0, search_response.Context.Returned, "search geometry")
+	expectedType := "FeatureCollection"
+	if searchResponse.Type != expectedType {
+		t.Errorf("Expected type %s, but got %s", expectedType, searchResponse.Type)
+	}
+
+	expectedLimit := 100
+	if searchResponse.Context.Limit != expectedLimit {
+		t.Errorf("Expected limit %d, but got %d", expectedLimit, searchResponse.Context.Limit)
+	}
+
+	expectedReturned := 0
+	if searchResponse.Context.Returned != expectedReturned {
+		t.Errorf("Expected returned %d, but got %d", expectedReturned, searchResponse.Context.Returned)
+	}
 }
