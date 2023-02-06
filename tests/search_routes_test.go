3790f2cdb2f307c981b818c148bab9bd34147a0b
jonhealy1 <jonathan.d.healy@gmail.com>
2023-02-06 17:10:44 +0300

    bbox no results

--- tests/search_routes_test.go
+++ tests/search_routes_test.go
@@ -794,10 +794,58 @@ func TestPostSearchBbox3d(t *testing.T) {
 	}
 }
 
-// func TestPostSearchBbox3d(t *testing.T) {
+func TestPostSearchBboxNoResults(t *testing.T) {
+	jsonBody := []byte(`{
+		"collections": ["sentinel-s2-l2a-cogs-test"],
+		"bbox": [17.504892,-75.254738,19.321298,-65.431580]
+	}`)
+	bodyReader := bytes.NewReader(jsonBody)
+
+	app := Setup()
+	req, _ := http.NewRequest("POST", "/search", bodyReader)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := app.Test(req, -1)
+	if err != nil {
+		t.Fatalf("An error occurred: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		t.Fatalf("Expected status code 200, but got %d", resp.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("An error occurred: %v", err)
+	}
+
+	var searchResponse responses.SearchResponse
+	err = json.Unmarshal(body, &searchResponse)
+	if err != nil {
+		t.Fatalf("An error occurred: %v", err)
+	}
+
+	expectedType := "FeatureCollection"
+	if searchResponse.Type != expectedType {
+		t.Errorf("Expected type %s, but got %s", expectedType, searchResponse.Type)
+	}
+
+	expectedLimit := 100
+	if searchResponse.Context.Limit != expectedLimit {
+		t.Errorf("Expected limit %d, but got %d", expectedLimit, searchResponse.Context.Limit)
+	}
+
+	expectedReturned := 0
+	if searchResponse.Context.Returned != expectedReturned {
+		t.Errorf("Expected returned %d, but got %d", expectedReturned, searchResponse.Context.Returned)
+	}
+}
+
+// func TestPostSearchBboxNoResults(t *testing.T) {
 // 	jsonBody := []byte(`{
 // 		"collections": ["sentinel-s2-l2a-cogs-test"],
-// 		"bbox": [97.504892,-75.254738, 0, 179.321298,-65.431580, 0]
+// 		"bbox": [17.504892,-75.254738,19.321298,-65.431580]
 // 	}`)
 // 	bodyReader := bytes.NewReader(jsonBody)
 
@@ -823,41 +871,8 @@ func TestPostSearchBbox3d(t *testing.T) {
 
 // 	assert.Equalf(t, "FeatureCollection", search_response.Type, "search geometry")
 // 	assert.Equalf(t, 100, search_response.Context.Limit, "search geometry")
-// 	assert.Equalf(t, 50, search_response.Context.Returned, "search geometry")
-// 	assert.Equalf(t, "sentinel-s2-l2a-cogs-test", search_response.Features[0].Collection, "search collections")
+// 	assert.Equalf(t, 0, search_response.Context.Returned, "search geometry")
 // }
-
-func TestPostSearchBboxNoResults(t *testing.T) {
-	jsonBody := []byte(`{
-		"collections": ["sentinel-s2-l2a-cogs-test"],
-		"bbox": [17.504892,-75.254738,19.321298,-65.431580]
-	}`)
-	bodyReader := bytes.NewReader(jsonBody)
-
-	resp, err := http.Post(
-		"http://localhost:6002/search",
-		"application/json",
-		bodyReader,
-	)
-	if err != nil {
-		log.Fatalf("An Error Occured %v", err)
-	}
-	defer resp.Body.Close()
-
-	assert.Equalf(t, 200, resp.StatusCode, "create item")
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	var search_response responses.SearchResponse
-	json.Unmarshal(body, &search_response)
-
-	assert.Equalf(t, "FeatureCollection", search_response.Type, "search geometry")
-	assert.Equalf(t, 100, search_response.Context.Limit, "search geometry")
-	assert.Equalf(t, 0, search_response.Context.Returned, "search geometry")
-}
 
 func TestGetSearchBbox(t *testing.T) {
 
