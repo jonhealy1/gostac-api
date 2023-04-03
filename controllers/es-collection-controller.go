c50e0ad3099e093b83dfa78dbedff875c3e280cd
jonhealy1 <jonathan.d.healy@gmail.com>
2023-04-03 10:36:42 +0300

    create es collection controller

---
+++ controllers/es-collection-controller.go
@@ -0,0 +1,79 @@
+package controllers
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"go-stac-api-postgres/database"
+
+	"github.com/go-playground/validator"
+	"github.com/gofiber/fiber/v2"
+	"github.com/olivere/elastic/v7"
+
+	"go-stac-api-postgres/models"
+)
+
+func CreateESCollection(c *fiber.Ctx) error {
+	stac_collection := new(models.StacCollection)
+	err := c.BodyParser(&stac_collection)
+	if err != nil {
+		c.Status(http.StatusUnprocessableEntity).JSON(
+			&fiber.Map{"message": "request failed"})
+		return err
+	}
+
+	collection := models.Collection{
+		Data: models.JSONB{(&stac_collection)},
+		Id:   stac_collection.Id,
+	}
+	validator := validator.New()
+	err = validator.Struct(collection)
+
+	if err != nil {
+		c.Status(http.StatusUnprocessableEntity).JSON(
+			&fiber.Map{"message": err},
+		)
+		return err
+	}
+
+	// Create Elasticsearch index if it doesn't exist
+	indexName := "collections"
+	_, err = database.ES.Client.CreateIndex(indexName).Do(context.Background())
+	if err != nil && !elastic.IsConflict(err) {
+		c.Status(http.StatusInternalServerError).JSON(
+			&fiber.Map{"message": "could not create Elasticsearch index"})
+		return err
+	}
+
+	// Index the collection document in Elasticsearch
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	doc, err := json.Marshal(collection)
+	if err != nil {
+		c.Status(http.StatusInternalServerError).JSON(
+			&fiber.Map{"message": "could not marshal collection"})
+		return err
+	}
+
+	resp, err := database.ES.Client.Index().
+		Index(indexName).
+		Id(collection.Id).
+		BodyString(string(doc)).
+		Do(ctx)
+
+	if err != nil {
+		c.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "could not index collection"})
+		return err
+	}
+
+	c.Status(http.StatusCreated).JSON(&fiber.Map{
+		"message":         "success",
+		"id":              resp.Id,
+		"stac_collection": collection.Data[0],
+	})
+	return nil
+}
