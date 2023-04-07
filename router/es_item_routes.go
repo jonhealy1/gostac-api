787ca487c003acd55d09cdfcfd9339e2ce5dcfc6
jonhealy1 <jonathan.d.healy@gmail.com>
2023-04-07 15:23:05 +0300

    add item collection route

--- router/es_item_routes.go
+++ router/es_item_routes.go
@@ -9,7 +9,7 @@ import (
 func ESItemRoute(app *fiber.App) {
 	app.Post("/es/collections/:collectionId/items", controllers.ESCreateItem)
 	app.Get("/es/collections/:collectionId/items/:itemId", controllers.ESGetItem)
-	// app.Get("/es/collections/:collectionId/items", controllers.ESGetItemCollection)
+	app.Get("/es/collections/:collectionId/items", controllers.ESGetItemCollection)
 	app.Put("/es/collections/:collectionId/items/:itemId", controllers.ESUpdateItem)
 	app.Delete("/es/collections/:collectionId/items/:itemId", controllers.ESDeleteItem)
 }
