edd2054ba2e0fce11434bce59494c6c1fbb1cede
jonhealy1 <jonathan.d.healy@gmail.com>
2023-04-12 14:54:26 +0300

    refactor

--- es-api/controllers/controller-helpers.go
+++ es-api/controllers/controller-helpers.go
@@ -2,26 +2,28 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
-	"github.com/gofiber/fiber/v2"
 	"github.com/jonhealy1/goapi-stac/es-api/models"
 )
 
 func CreateCollectionFromMessage(msg *kafka.Message) {
 	// Parse the message to get the collection
 	var stac_collection models.StacCollection
+	fmt.Println("Message: ", string(msg.Value))
 	err := json.Unmarshal(msg.Value, &stac_collection)
 	if err != nil {
 		log.Printf("Error unmarshalling collection from Kafka message: %v\n", err)
 		return
 	}
 
-	// Call CreateESCollection with the parsed collection
-	c := &fiber.Ctx{} // Create an empty context
-	err = CreateESCollection(c, &stac_collection)
+	// Call CreateESCollectionCore with the parsed collection
+	collection, err := CreateESCollectionCore(&stac_collection)
 	if err != nil {
 		log.Printf("Error creating ES collection from Kafka message: %v\n", err)
+	} else {
+		log.Printf("ES collection created successfully: %s\n", collection.Id)
 	}
 }
