1b5656deae42640f4fd0d5884a9acf2c3e98788d
jonhealy1 <jonathan.d.healy@gmail.com>
2023-04-13 10:40:47 +0300

    update collection helper

--- es-api/controllers/controller-helpers.go
+++ es-api/controllers/controller-helpers.go
@@ -20,10 +20,30 @@ func CreateCollectionFromMessage(msg *kafka.Message) {
 	}
 
 	// Call CreateESCollectionCore with the parsed collection
-	collection, err := CreateESCollectionCore(&stac_collection)
+	collection, err := UpsertESCollectionCore(false, stac_collection.Id, &stac_collection)
 	if err != nil {
 		log.Printf("Error creating ES collection from Kafka message: %v\n", err)
 	} else {
 		log.Printf("ES collection created successfully: %s\n", collection.Id)
+	}
+}
+
+func UpdateCollectionFromMessage(msg *kafka.Message) {
+	fmt.Println("UpdateCollectionFromMessage")
+	// Parse the message to get the collection
+	var stac_collection models.StacCollection
+	fmt.Println("Message: ", string(msg.Value))
+	err := json.Unmarshal(msg.Value, &stac_collection)
+	if err != nil {
+		log.Printf("Error1 unmarshalling collection from Kafka message: %v\n", err)
+		return
+	}
+
+	// Call UpdateESCollectionCore with the parsed collection
+	collection, err := UpsertESCollectionCore(true, stac_collection.Id, &stac_collection)
+	if err != nil {
+		log.Printf("Error updating ES collection from Kafka message: %v\n", err)
+	} else {
+		log.Printf("ES collection updated successfully: %s\n", collection.Id)
 	}
 }
